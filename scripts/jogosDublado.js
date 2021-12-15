f3ad500a418e7115a3c4aeb78f33b2a55667fda9
Marcos Vinicius <91851179+Marcospicko@users.noreply.github.com>
2021-12-15 15:04:56 -0300

    Update jogosDublado.js

--- scripts/jogosDublado.js
+++ scripts/jogosDublado.js
@@ -1,57 +1,60 @@
-///declaração anonima
+'use strict';
 
-let baixandoJogos = function () {
-    window.alert("Bom aproveito , baixe os melhores jogos  que marcaram sua vida")
+// usa as marcaçoes feita no radio em uma array  (não feita)
+
+
+// declaração anonima
+const baixandoJogos = function() {
+  window.alert('Bom aproveito , baixe os melhores jogos  que marcaram sua vida');
 }
-    baixandoJogos ();
 
-    //return
+//  return
 function cliqueEmImagens() {
-        return alert("Por favor para fazer o download clique no texto ao lado da imagem");
-}
- //função auto executavel
-     ( function () {
-       let anoNascimento =2021-prompt("Digite o ano que vc nasceu para que possamos saber se vc tem mais de 10 anos  de idade para poder baixar jogos neste site !");
-     
-        if(anoNascimento >10) {
-            window.alert("Beleza coleguinha você esta apto para baixar os jogos neste site !"); 
-        } 
-        else if(anoNascimento <10) {
-            window.alert("aaaaa que pena que você não tem idade suficientea  a idade mínima para entrar neste site é 10 anos, mas não fique triste vamos te levar para um site aonde você  pode assitir desenhos para sua idade "); 
-            window.open('https://www.youtube.com/watch?v=Fn9adh4HWUU');
-        }
-    }
-  )();
-
-
-
-//Evento de carregamento do documento - onload
-
-function checkCookies()
-{
-if (navigator.cookieEnabled==true)
-	{
-	alert("Cookies são permitidos")
-	}
-else
-	{
-	alert("Cookies não são permitidos")
-	}
+  return window.alert('Por favor para fazer o download clique no texto ao lado da imagem');
 }
 
-///////////////////////////////////////////////////
+// função auto executavel
+(function() {
+  const anoAtual = 2021;
 
-            //////// getElementsByName()//////////
-            
-let btn = document.getElementById('btnRate');
+  const anoNascimento = anoAtual - window.prompt('Digite o ano que vc nasceu para que possamos '+
+   'saber se vc tem mais de 10 anos de idade para poder baixar jogos neste site !');
+
+  if (anoNascimento > 10) {
+    window.alert('Beleza coleguinha você esta apto para baixar os jogos neste site !');
+  } else if (anoNascimento < 10 || anoNascimento === 0) {
+    window.alert('aaaaa que pena que você não tem idade suficientea  a idade mínima para '+
+                'entrar neste site é 10 anos, mas não fique triste vamos te lelet para um '+
+                'site aonde você  pode assitir desenhos para sua idade ');
+
+    window.open('https://www.youtube.com/watch?v=Fn9adh4HWUU');
+  }
+}());
+
+//  Evento de carregamento do documento - onload
+ function checkCookies() {
+  if (navigator.cookieEnabled === true) {
+    window.alert('Cookies são permitidos');
+  } else {
+    window.alert('Cookies não são permitidos');
+  }
+}
+
+//  getElementsByName
+const btn = document.getElementById('botao');
 
 btn.addEventListener('click', () => {
-    let cotacao = document.getElementsByName('avaliar');
-    cotacao.forEach((avaliar) => {
-        if (avaliar.checked) {
-            alert('O jogo que vc escolheu no caso: '+avaliar.value+' será contabilizado nos votos');
-        }
-    })
-});
+  const contagem = document.getElementsByName('avaliar');
+  contagem.forEach((avaliar) => {
+   if (avaliar.checked) {
+     window.alert('O jogo que vc escolheu no caso: ' + avaliar.value + 
+     ' será contabilizado nos votos');
+    }
+  })
+}); 
 
-
+// Efeitos fade ou slide
+$('#toggle-sliding').click(function() {
+	$('form').slideToggle('slow');
+  });
+  
